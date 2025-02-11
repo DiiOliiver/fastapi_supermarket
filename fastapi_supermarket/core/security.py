7a056ddf612f4499194c4bf37a4ec76f50e1ed6d
Diego Feitosa de Oliveira <diego.feitosa.oliveira@hotmail.com>
2025-02-11 09:42:22 -0400

    refactor: desacoplado variáveis de ambiente do código

--- fastapi_supermarket/core/security.py
+++ fastapi_supermarket/core/security.py
@@ -7,17 +7,15 @@ from fastapi.security import OAuth2PasswordBearer
 from jwt import DecodeError, decode, encode
 from pwdlib import PasswordHash
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
 from fastapi_supermarket.annotaded.t_session import T_Session
+from fastapi_supermarket.core.settings import Settings
 from fastapi_supermarket.models import User
+
+settings = Settings()
 
 pwd_context = PasswordHash.recommended()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
-
-SECRET_KEY = 'your-secret-key'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def get_password_hash(password: str) -> str:  # pragma: no cover
@@ -33,10 +31,12 @@ def verify_password(
 def create_access_token(data_payload: dict) -> dict:
     to_encode = data_payload.copy()
     expire = datetime.now(tz=ZoneInfo('America/Manaus')) + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
     to_encode.update({'exp': expire})
-    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
     return encoded_jwt
 
 
@@ -51,7 +51,9 @@ def get_current_user(
     )
 
     try:
-        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
         subject_email: str = payload.get('sub')
         if not subject_email:
             raise credentials_exception
--- tests/core/test_security.py
+++ tests/core/test_security.py
@@ -1,15 +1,16 @@
 from jwt import decode
 
 from fastapi_supermarket.core.security import (
-    ALGORITHM,
-    SECRET_KEY,
     create_access_token,
+    settings,
 )
 
 
 def test_create_access_token_success():
     data = {'sub': 'teste@test.com'}
     token = create_access_token(data)
-    decoded = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    decoded = decode(
+        token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+    )
     assert decoded['sub'] == data['sub']
     assert decoded['exp'] is not None
